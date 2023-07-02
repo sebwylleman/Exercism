901f44c4a7bfb8058ae2219b2b39576cf63ce1ca
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2023-07-02 06:58:34 +0100

    solution

--- javascript/lucky-numbers/lucky-numbers.js
+++ javascript/lucky-numbers/lucky-numbers.js
@@ -8,7 +8,7 @@
  * @returns {number} sum of the two arrays
  */
 export function twoSum(array1, array2) {
-  throw new Error('Implement the twoSum function');
+  return Number(array1.join('')) + Number(array2.join(''));
 }
 
 /**
@@ -18,7 +18,8 @@ export function twoSum(array1, array2) {
  * @returns {boolean} whether the number is a palindrome or not
  */
 export function luckyNumber(value) {
-  throw new Error('Implement the luckyNumber function');
+  const str = String(value);
+  return str === str.split('').reverse().join('');
 }
 
 /**
@@ -29,5 +30,11 @@ export function luckyNumber(value) {
  * @returns {string} error message
  */
 export function errorMessage(input) {
-  throw new Error('Implement the errorMessage function');
+  if (!input) {
+    return 'Required field';
+  }
+  if (input && Number(input)) {
+    return '';
+  }
+  return 'Must be a number besides 0';
 }
