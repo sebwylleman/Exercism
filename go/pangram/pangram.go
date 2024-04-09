60ca92e91e293927d5a2a7b09b743a641699111e
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2024-04-09 11:38:55 +0100

    pangram refactored to be more efficient

--- go/pangram/pangram.go
+++ go/pangram/pangram.go
@@ -6,12 +6,12 @@ import (
 )
 
 func IsPangram(input string) bool {
-	seenLetters := map[rune]bool{}
+	seenLetters := map[rune]struct{}{}
 	lowercaseInput := strings.ToLower(input)
 
 	for _, char := range lowercaseInput {
 		if unicode.IsLetter(char) {
-			seenLetters[char] = true
+			seenLetters[char] = struct{}{}
 		}
 		// 26 characters in the English alphabet
 		if len(seenLetters) == 26 {
