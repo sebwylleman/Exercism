161196d0001a79336d3635ca6f7171de97ddf8dd
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2024-04-15 09:59:08 +0100

    refactor: simpler solution without the need to create an extra map data type

--- go/resistor-color/resistor_color.go
+++ go/resistor-color/resistor_color.go
@@ -1,7 +1,5 @@
 package resistorcolor
 
-var resistorValue = make(map[string]int)
-
 // Colors returns the list of all colors.
 func Colors() []string {
 	return []string{
@@ -20,15 +18,10 @@ func Colors() []string {
 
 // ColorCode returns the resistance value of the given color.
 func ColorCode(color string) int {
-
-	resistorValue := make(map[string]int)
 	for i, v := range Colors() {
-		resistorValue[v] = i
+		if v == color {
+			return i
+		}
 	}
-
-	value, ok := resistorValue[color]
-	if !ok {
-		return 0
-	}
-	return value
+	return -1
 }
