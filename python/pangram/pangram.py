998f00e4f1a2015b76de558b9f8e0692943cb78e
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2024-06-13 08:46:50 +0100

    refactor: better readability. No need to create a set() data structure

---
+++ python/pangram/pangram.py
@@ -0,0 +1,8 @@
+from string import ascii_lowercase
+
+def is_pangram(sentence):
+
+    for char in ascii_lowercase:
+        if char not in sentence.lower():
+            return False     
+    return True  
